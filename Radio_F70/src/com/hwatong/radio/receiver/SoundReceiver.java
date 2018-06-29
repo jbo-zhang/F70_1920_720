898ea1941865ad83a706f44f443e5991c0d69cd6
zhangjinbo <zhangjinbo@hwatong.com.cn>
2018-06-29 14:20:40 +0800

    Radio_F70: 打开调频，打开FM添加

--- Radio_F70/src/com/hwatong/radio/receiver/SoundReceiver.java
+++ Radio_F70/src/com/hwatong/radio/receiver/SoundReceiver.java
@@ -13,16 +13,18 @@ public class SoundReceiver extends BroadcastReceiver{
 	private final static String thiz = SoundReceiver.class.getSimpleName();
 	
 	private static final String OPEN_AM = "com.hwatong.voice.OPEN_AM";
+	private static final String OPEN_FM = "com.hwatong.voice.OPEN_FM";
 	private static final String FM_CMD_ACTION = "com.hwatong.voice.FM_CMD";
 	private static final String AM_CMD_ACITON = "com.hwatong.voice.AM_CMD";
 	
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		L.d(thiz, "onReceive action : " + intent.getAction());
-		if(OPEN_AM.equals(intent.getAction())) {
+		if(OPEN_AM.equals(intent.getAction()) || OPEN_FM.equals(intent.getAction())) {
 			Intent activityIntent = new Intent(context, Radio.class);
 			activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			activityIntent.putExtra("type", 1);
+			int type = OPEN_AM.equals(intent.getAction()) ? 1 : 0;
+			activityIntent.putExtra("type", type);
 			context.startActivity(activityIntent);
 		} else if(FM_CMD_ACTION.equals(intent.getAction()) || AM_CMD_ACITON.equals(intent.getAction())){
 			L.d(thiz, "CMD_ACTION");
